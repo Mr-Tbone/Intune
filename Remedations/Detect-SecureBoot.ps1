e273c419321db7efd84b7e2412c4b34a38a70de8
Mr T-Bone <torbjorn.granheden@outlook.com>
2026-01-09 13:15:42 +0100

    Detect secure boot

--- Remedations/Detect-SecureBoot.ps1
+++ Remedations/Detect-SecureBoot.ps1
@@ -15,7 +15,7 @@
     Script for Intune remediation to detect if Secure Boot is enabled
 
 .DESCRIPTION
-    The script detects if Secure Boot is enabled on the device.
+    The script detects if Secure Boot is enabled on the device and also collects diagnostic info on the device.
     Returns Compliant (exit 0) if enabled, Non-Compliant (exit 1) if disabled or not supported.
     The compact function invoke-TboneTinyLog is used to capture logs and return them to Intune and disk for troubleshooting.
 
